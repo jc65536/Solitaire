c48dee0f5bdcd261e96c333109dbb9eb2c76363d
Jason Cheng <jasoncheng224@gmail.com>
2022-06-18 14:16:17 -0700

    Initial commit

---
+++ app/src/main/java/site/jcfp/solitaire/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package site.jcfp.solitaire.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
